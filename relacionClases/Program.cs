d51fc35c409eb92e83223fa3d0d5a3ddb6aee77a
allStar-glitch <lialora3@gmail.com>
2025-09-27 16:50:31 -0400

    Primer Commit

---
+++ relacionClases/Program.cs
@@ -0,0 +1,41 @@
+﻿// Ejemplo de Agregación
+class Pez
+{
+    public string Nombre;
+    public Pez(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
+
+class Acuario
+{
+    public List<Pez> Peces = new List<Pez>();
+
+    public void AgregarPez(Pez p)
+    {
+        Peces.Add(p);
+    }
+}
+
+// Ejemplo de Composición
+class Aleta
+{
+    public string Lado;
+    public Aleta(string lado)
+    {
+        Lado = lado;
+    }
+}
+
+class Orca
+{
+    private Aleta aletaIzq;
+    private Aleta aletaDer;
+
+    public Orca()
+    {
+        aletaIzq = new Aleta("Izquierda");
+        aletaDer = new Aleta("Derecha");
+    }
+}
